03e9c7dfa13c21ecb51f076fb3f0bd8cbdffcec1
sjpbailey <sjpbailey1961@gmail.com>
2021-03-15 16:28:53 -0700

    Update BasEdgeController.py

--- nodes/BasEdgeController.py
+++ nodes/BasEdgeController.py
@@ -14,7 +14,7 @@ import time
 import requests
 from requests.auth import HTTPBasicAuth
 import xml.etree.ElementTree as ET
-from nodes import bascontrolns
+import bascontrolns
 from bascontrolns import Device, Platform
 
 # My Template Node
